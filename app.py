6e8b536e30096560d2b5ed078cfda0520d77adbd
Soral-H <soral.halabi@hotmail.com>
2025-04-27 17:31:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,5 @@ import os
 from rasa import run
 
 if __name__ == "__main__":
-    # Set the Python version if needed (optional)
-    os.environ["PYTHON_VERSION"] = "3.9.13"  # Set your Python version
-
-    # Start the Rasa server with the specified options
-    run(
-        enable_api=True,  # Enable the API
-        cors="*",         # Allow all origins for CORS
-        port=5005         # Default port for Rasa server
-    )
+    port = os.environ.get("PORT", 5005)  # Default to 5005 if PORT is not set
+    run(port=port)
